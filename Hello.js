e33d85e1063d0740c4a4628d03773d1a713e3201
jeremiahhappiness998-afk <jeremiahhappiness998@gmail.com>
2025-10-17 21:48:11 +0100

    Add simple calculator in Hello.js

---
+++ Hello.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction to JavaScript</title>
+</head>
+<body>
+    <script>
+        // a simple calculator
+        let a = 5;
+        let b = 5;
+        let sum = sum(a,b);
+
+        // console result
+        console.log(sum)
+        
+        function sum(a, b){
+            return a + b;
+        }
+    </script>
+</body>
+</html>
